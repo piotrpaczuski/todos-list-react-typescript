0670aa4c7257d8d475c8293a6dbe45bc760e7229
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-05-02 05:46:02 +0200

    Add TypeScript to Section component

--- src/common/Section/index.tsx
+++ src/common/Section/index.tsx
@@ -1,6 +1,13 @@
 import { StyledSection, StyledSectionHeader } from "./styled";
+import React from "react";
 
-const Section = ({ title, extraContent, body }) => (
+interface SectionProps {
+    title: string;
+    extraContent?: React.ReactNode;
+    body: React.ReactNode;
+}
+
+const Section: React.FC<SectionProps> = ({ title, extraContent, body }: SectionProps) => (
     <StyledSection>
         <StyledSectionHeader>
             <h2>{title}</h2>
