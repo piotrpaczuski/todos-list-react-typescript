82db9b6ca358e719f9d945d4fe6ea212f513814a
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-05-06 23:54:54 +0200

    Add TypeScript to TasksPage component

--- src/features/tasks/TasksPage/index.tsx
+++ src/features/tasks/TasksPage/index.tsx
@@ -1,27 +1,22 @@
-import Form from "./Form";
-import TaskList from "./TaskList";
+import React from "react";
+
 import { Buttons, ExampleTasksButton } from "./Buttons";
-import Section from "../../../common/Section";
+import Form from "./Form";
 import Header from "../../../common/Header";
+import Section from "../../../common/Section";
 import Search from "./Search";
+import TaskList from "./TaskList";
 
-function TasksPage() {
-
-
+function TasksPage(): JSX.Element {
   return (
     <>
-      <Header
-        header={"Lista zadań"}
-      />
+      <Header header={"Lista zadań"} />
       <Section
         title={"Dodaj nowe zadanie"}
         body={<Form />}
         extraContent={<ExampleTasksButton />}
       />
-      <Section
-        title={"Wyszukiwanie"}
-        body={<Search />}
-      />
+      <Section title={"Wyszukiwanie"} body={<Search />} />
       <Section
         title={"Lista zadań"}
         extraContent={<Buttons />}
