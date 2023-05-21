cb8d06fbbb6d8f3cb7131768fcaa3350abf3a468
Piotr Paczuski <piotr-paczuski@o2.pl>
2023-05-21 12:16:49 +0200

    Use a new interface name and a different getItem type

--- src/features/tasks/tasksLocalStorage.ts
+++ src/features/tasks/tasksLocalStorage.ts
@@ -1,9 +1,9 @@
-import { TasksTypes } from "./tasksTypes";
+import { Task } from "./tasksTypes";
 
-const nameOfKey: string = "tasks";
+const nameOfKey = "tasks";
 
-export const setTasksLocalStorage = (tasks: TasksTypes[]) =>
+export const setTasksLocalStorage = (tasks: Task[]) =>
   localStorage.setItem(nameOfKey, JSON.stringify(tasks));
 
-export const getTasksLocalStorage = (): TasksTypes[] =>
-  JSON.parse(localStorage.getItem(nameOfKey) as string) || [];
+export const getTasksLocalStorage = (): Task[] =>
+  JSON.parse(localStorage.getItem(nameOfKey)?.toString() ?? "") || [];
